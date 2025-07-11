9e50d90b19179af50f8ba3e9742207b042f681e3
niharika singh <nonisingh991@gmail.com>
2025-07-11 08:30:43 +0000

    new filessss

---
+++ basics/01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 12343211;
+let accountEmail = "niharika1912singh@gmail.com";
+var accountPassword = "12345";
+accountCity = "jaipur";
+let accountState;
+//console.log(accountId)
+/*prefer not to use var
+because of issue in block scope and functional scope*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ basics/01_basics/02_datatypes.js
@@ -0,0 +1,8 @@
+"use strict";//treat all JS code as newer version
+
+//alert("yo bro what is up");//we are using node.js not browser
+let name = "hitesh";
+let age = 18;
+let isLoggedIn = false;
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
