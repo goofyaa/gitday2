dc73b9194af6d6eeeb3992812959a8c324e97902
niharika singh <nonisingh991@gmail.com>
2025-07-11 10:29:29 +0000

    refactor: clean up code formatting and comments

---
+++ basics/01_basics/04_comparison.js
@@ -0,0 +1,12 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2<=1); 
+// // ans will be true or false
+
+
+// // problem in comparing diff dta types
+// //console.log("2" > 1); this is wrong else use type script
+console.table([null > 0,null == 0,null >= 0]);
+// // strict check
+console.log("2" === 2);
