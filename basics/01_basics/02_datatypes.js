61aa5dd4424cbb6b348ef4c02a2597974372492c
niharika singh <nonisingh991@gmail.com>
2025-07-11 09:05:07 +0000

    new changes

--- basics/01_basics/02_datatypes.js
+++ basics/01_basics/02_datatypes.js
@@ -5,4 +5,4 @@ let name = "hitesh";
 let age = 18;
 let isLoggedIn = false;
 console.log(typeof null);//object
-console.log(typeof undefined);//undefined
+console.log([typeof undefined]);//undefined
---
+++ basics/01_basics/03_ConversionOperation.js
@@ -0,0 +1,23 @@
+let score = "33a";
+let Score = "33";
+let x = undefined;
+//console.log(typeof score);
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//if it is not an actual number , the type will still be converted 
+//console.log(valueInNumber);
+//but the value will be NaN 
+//let valueIn = Number(x);
+//console.table([typeof valueIn , valueIn]);
+// let isLoggedIn = 1;
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+// 1 = true
+// 0 = false
+// "" = false
+// "niharika" = true
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
