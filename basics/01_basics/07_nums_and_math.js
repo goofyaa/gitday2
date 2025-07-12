dfa9cf71bf80bd7e9d297bf26d3981fdf340044d
niharika singh <nonisingh991@gmail.com>
2025-07-13 00:26:42 +0530

    ronaldo suiii

---
+++ basics/01_basics/07_nums_and_math.js
@@ -0,0 +1,23 @@
+// const score = 400;
+// console.log(score);
+// const balance = new Number(100.2323);//specifically a number type
+// console.log(balance);
+// const otherNumber = 123.8394;
+// console.log(balance.toString());//adds more properties related to string
+// console.log(balance.toFixed(2));//number after decimal
+// console.log(otherNumber.toPrecision(4));//returns a string of number with 4 digits (baaki uda dega digits)
+//************************math*********************
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.2));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.2));
+// console.log(Math.max(6,8,99,2));
+// console.log(Math.min(6,8,99,2));
+// console.log(Math.random());//random no between 0 to 1
+// console.log((Math.random()*10)+1);
+const min = 10;
+const max = 20;
+console.log((Math.floor(Math.random()*(max-min +1)) + min));
